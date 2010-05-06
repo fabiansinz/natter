9b1e1289d194df80f4ac44fc4804ae427e671c28
sgerwinn <sgerwinn@7650456f-3baa-db11-867e-00304871138e>
2010-05-06 16:51:06 +0000

    git-svn-id: http://svn.tuebingen.mpg.de/cvn/code/python/natter@2499 7650456f-3baa-db11-867e-00304871138e

---
+++ Distribution/UnnormalizedGaussian.py
@@ -0,0 +1,35 @@
+from Distribution.Distribution import Gaussian
+from numpy import array,hstack,squeeze
+
+class UnnormalizedGaussian(Gaussian):
+    """
+    Unnormalized Gaussian distribution the normalization constant is
+    set via an explicit (primary) parameter. Primary parameters are
+    the parameters w.r.t which we can do inference.
+
+    Parameters:
+        n     : dimensionality
+        mu    : mean
+        sigma : covariance
+        Z     : normalization constant
+
+    Primary parameters:
+        Z
+    """
+
+    def __init__(self,param=None):
+        Gaussian.__init__(self,param)
+        self.name = "Unnormalized Gaussian"
+        self.param['Z'] = 1.0;
+        if param.has_key('Z'):
+            self.param['Z'] = param['Z']
+        self.primary = ['Z']
+
+    def primary2array(self):
+        return self.param['Z']
+
+    def array2primary(self,arr):
+        if len(arr)!=1:
+            raise DimensionalityError("Normalization constant has to be scalar!")
+        self.param['Z']= arr[0]
+    
---
+++ Tests/TestNoiseContrastive.py
@@ -0,0 +1,16 @@
+from Auxiliary.Estimation import noiseContrastive
+
+
+class TestNoiseContrastive(unittest.TestCase):
+    nSamples = 1000;
+    
+    
+
+##################################################################################################
+
+if __name__=="__main__":
+    
+    unittest.main()
+
+
+    
