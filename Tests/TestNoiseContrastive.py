92a8f1b7373a577860140ad49f7184dee96450a4
sgerwinn <sgerwinn@7650456f-3baa-db11-867e-00304871138e>
2010-05-11 13:08:35 +0000

    git-svn-id: http://svn.tuebingen.mpg.de/cvn/code/python/natter@2505 7650456f-3baa-db11-867e-00304871138e

--- Tests/TestNoiseContrastive.py
+++ Tests/TestNoiseContrastive.py
@@ -1,10 +1,28 @@
 from Auxiliary.Estimation import noiseContrastive
-
+from Distribution import Gaussian
+from numpy import zeros,eye,ones
+from numpy.random import randn
+from numpy.linalg import norm
+import unittest
 
 class TestNoiseContrastive(unittest.TestCase):
-    nSamples = 1000;
-    
-    
+    def test_estimate(self):
+        nSamples = 1000;
+        modelParam = {'n': 2,'mu':ones(2),'sigma': eye(2)}
+        modelDist = Gaussian(param=modelParam)
+        trueArr = modelDist.primary2array();
+        data = modelDist.sample(nSamples)
+        mustart = randn(2)
+        modelDist.setParam('mu',mustart)
+        noiseContrastive(modelDist,data,verbosity=1)
+        estimateArr = modelDist.primary2array()
+        self.assertTrue(norm(trueArr - estimateArr)<= 1e-01)
+        
+        
+        
+
+
+
 
 ##################################################################################################
 
