3ba5dcff4925a7b6a0f1cc5558a17c0bafa049f7
sgerwinn <sgerwinn@7650456f-3baa-db11-867e-00304871138e>
2010-05-11 13:28:01 +0000

    git-svn-id: http://svn.tuebingen.mpg.de/cvn/code/python/natter@2508 7650456f-3baa-db11-867e-00304871138e

--- Filter/__init__.py
+++ Filter/__init__.py
@@ -1,5 +1,4 @@
-from Filter import Filter
+from Filter import Filter,load
 from LinearFilter import LinearFilter
 from NonlinearFilter import NonlinearFilter
-from Filter import load
 import FilterFactory
