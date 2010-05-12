d868d88353333debc834b6158a1611567bbf5f7c
fabee <fabee@7650456f-3baa-db11-867e-00304871138e>
2010-05-12 06:17:59 +0000

    git-svn-id: http://svn.tuebingen.mpg.de/cvn/code/python/natter@2536 7650456f-3baa-db11-867e-00304871138e

--- Examples/plotHistogram.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import Transform
-import Distributions
-
-if __name__=="__main__":
-    p = Distributions.GammaP({'u':np.random.rand()*5.0,'s':np.random.rand()*10.0,'p':np.random.rand()*1.5+.5})
-    print p
-    dat = p.sample(10000)
-    p.histogram(dat)
