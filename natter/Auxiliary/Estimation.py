b4242823fdb470784c77d62073d53f8c84000f90
sgerwinn <sgerwinn@7650456f-3baa-db11-867e-00304871138e>
2010-05-17 06:56:00 +0000

    git-svn-id: http://svn.tuebingen.mpg.de/cvn/code/python/natter@2560 7650456f-3baa-db11-867e-00304871138e

--- natter/Auxiliary/Estimation.py
+++ natter/Auxiliary/Estimation.py
@@ -1,8 +1,9 @@
 from __future__ import division
 from natter.Distributions import Gaussian
 from scipy import optimize
-from numpy import sum,log,exp
-
+from numpy import sum,log,exp,linspace,array,logspace
+import sys
+import pylab as pl
 
 
 
@@ -31,18 +32,27 @@ def noiseContrastive(modelDistribution,
     def f(theta):
         modelDistribution.array2primary(theta)
         return noiseContrastiveObjective(modelDistribution,noiseDistribution,data,dataNoise)
+    
     def df(theta):
         modelDistribution.array2primary(theta)
         return gradNoiseContrastiveObjective(modelDistribution,noiseDistribution,data,dataNoise)
-    def check(theta):
-        err = optimize.check_grad(f,df,theta)
-        print "error from check_grad: " , err
+
+    def checkG(theta):
+        err= optimize.check_grad(f,df,theta)
+        print "Err: " , err
+        sys.stdout.flush()
         
     theta0 = modelDistribution.primary2array()
+    # if len(theta0)==1:
+    #     ts = logspace(-1,1,50)
+    #     pl.loglog(ts,[f(array([t])) for t in ts])
+    #     pl.vlines(1.0,0.1,2)
+    #     pl.draw()
+    #     pl.show()
     if verbosity>0:
         print "VERBOSE!!"
-        check(theta0)
-        thetaOpt = optimize.fmin_bfgs(f,theta0,df,disp=1,callback = check)
+        thetaOpt = optimize.fmin_bfgs(f,theta0,df,gtol = 1e-08,disp=1)
+        thetaOpt = optimize.fmin_cg(f,thetaOpt,df,gtol = 1e-08,disp=1)
     else:
         thetaOpt = optimize.fmin_bfgs(f,theta0,df,disp=0)
     
@@ -63,7 +73,8 @@ def noiseContrastiveObjective(modelDistribution,noiseDistribution,dataModel,data
 
     hx =  logistic(modelDistribution.loglik(dataModel) -noiseDistribution.loglik(dataModel))
     hy =  logistic(modelDistribution.loglik(dataNoise) -noiseDistribution.loglik(dataNoise))
-    J = -1/(2*dataModel.size(0))*sum(log(hx) + log(1-hy))
+    n,m = dataModel.size()
+    J = -1/((2*m))*sum(log(hx) + log(1-hy))
     return J
 
 
@@ -80,11 +91,10 @@ def gradNoiseContrastiveObjective(modelDistribution,noiseDistribution,dataModel,
     """
     hx = logistic(modelDistribution.loglik(dataModel) -noiseDistribution.loglik(dataModel))
     hy = logistic(modelDistribution.loglik(dataNoise) -noiseDistribution.loglik(dataNoise))
-    print "Size hx: " + str(hx.shape)
-    print "size dldtheta " + str(modelDistribution.dldtheta(dataModel).shape)
     g1 = sum((1-hx)*modelDistribution.dldtheta(dataModel),axis=1)
     g2 = sum((-hy)*modelDistribution.dldtheta(dataNoise),axis=1)
-    return (g1+g2)
+    n,m = dataModel.size()
+    return (-1/(2*m))*(g1+g2)
 
 
 
@@ -94,7 +104,7 @@ def logistic(x):
 
     1/(1+exp(-x))
     """
-    return 1/(1+ exp(x))
+    return 1/(1+ exp(-x))
 
 
     
