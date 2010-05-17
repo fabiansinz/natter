aa1695b1d071976900be4addaed09d5b2190c332
fabee <fabee@7650456f-3baa-db11-867e-00304871138e>
2010-05-17 06:55:09 +0000

    git-svn-id: http://svn.tuebingen.mpg.de/cvn/code/python/natter@2559 7650456f-3baa-db11-867e-00304871138e

--- doc/source/Auxiliary_Entropy.rst
+++ doc/source/Auxiliary_Entropy.rst
@@ -4,3 +4,7 @@ The Entropy submodule
 .. automodule:: natter.Auxiliary.Entropy
    :members:	marginalEntropy
 
+
+
+.. [Scott1979] D.W. Scott, *On optimal and data-based histograms,* Biometrika,  vol. 66, Dez. 1979, S. 605-610.
+.. [Miller1955] G.A. Miller, *Note on the bias of information estimates,* Information Theory in Psychology: Problems and Methods, 1955, S. 95-100.
--- natter/Auxiliary/Entropy.py
+++ natter/Auxiliary/Entropy.py
@@ -37,8 +37,6 @@ def marginalEntropy(dat,method='JK'):
     :returns:   An numpy array containing the marginal entropies.
 
 
-    .. [Scott1979] D.W. Scott, *On optimal and data-based histograms,* Biometrika,  vol. 66, Dez. 1979, S. 605-610.
-    .. [Miller1955] G.A. Miller, *Note on the bias of information estimates,* Information Theory in Psychology: Problems and Methods, 1955, S. 95-100.
     
     """
 
