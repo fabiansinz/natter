8e58352b1744d02b0e1103195aa1ba85bf021c11
sgerwinn <sgerwinn@7650456f-3baa-db11-867e-00304871138e>
2010-05-18 18:10:39 +0000

    git-svn-id: http://svn.tuebingen.mpg.de/cvn/code/python/natter@2582 7650456f-3baa-db11-867e-00304871138e

--- Tests/TestFiniteMixtureDistribution.py
+++ Tests/TestFiniteMixtureDistribution.py
@@ -1,25 +1,33 @@
 from __future__ import division
-from natter.Distributions import FiniteMixtureDistribution
+from natter.Distributions import FiniteMixtureDistribution, FiniteMixtureOfGaussians
 from natter.Distributions import Gaussian
-from numpy import log,pi,sum,array
-from numpy.linalg import norm
+from numpy import log,pi,sum,array,ones,eye,sqrt
+from numpy.linalg import norm,cholesky,inv
 import unittest
 from scipy.optimize import check_grad, approx_fprime
 import numpy
-
+import pylab as pl
 
 
 class TestFiniteMixtureDistribution(unittest.TestCase):
     def setUp(self):
-        self.baseDistribution = Gaussian({'n':1})
-        self.baseDistribution.primary = ['mu']
+        dim = 2
+        self.baseDistribution = Gaussian({'n':dim})
+        self.baseDistribution.primary = ['sigma']
         self.numberOfMixtureComponents = 2;
-        self.nsamples = 2000
+        self.nsamples = 500
         self.mixture = FiniteMixtureDistribution(numberOfMixtureComponents=self.numberOfMixtureComponents,
                                                  baseDistribution=self.baseDistribution)
-        self.mixture.ps[1].param['mu'] = 15.0
+        self.mixture = FiniteMixtureOfGaussians(numberOfMixtureComponents=2,dim=dim,primary=['sigma'])
+        C1 =  eye(dim)*5 + ones((dim,dim))
+        C2 =  eye(dim)*5  -ones((dim,dim))
+        self.mixture.ps[0].param['sigma'] =C1
+        self.mixture.ps[0].cholP  = cholesky(inv(C1))
+        self.mixture.ps[1].param['sigma'] =C2
+        self.mixture.ps[1].cholP  = cholesky(inv(C2))
+        self.mixture.alphas = array([0.9,0.1])
         self.data = self.mixture.sample(self.nsamples)
-
+        self.ALL = sum(self.mixture.loglik(self.data))
     def test_init(self):
         pass
 
@@ -27,15 +35,15 @@ class TestFiniteMixtureDistribution(unittest.TestCase):
         """
         test, if we can learn the same parameters with which we generated the data.
         """
+        print self.data
         arrStart = self.mixture.primary2array()
-        # self.mixture.ps[0].param['mu'] =0.5
-        # self.mixture.ps[1].param['mu'] = 1.0
-        # self.mixture.alphas = array([0.8,0.2])
+        self.mixture.alphas = array([0.8,0.2])
         self.mixture.estimate(self.data,verbose=True)
         arrEnd   = self.mixture.primary2array()
         print "ground truth: " , arrStart
         print "ended with : ", arrEnd
-        print "Error in parameters: " , norm(arrStart-arrEnd)
+        ALL = sum(self.mixture.loglik(self.data))
+        print "Difference in ALL : " , abs(ALL - self.ALL)
 
 
  
