2b82b6b86b647a9bc30ae29aa90482120a029ef2
Fabian Sinz <fabee@tuebingen.mpg.de>
2011-07-26 09:46:43 +0200

    2nd derivative correctcd natter/cd natter/

--- natter/Distributions/PCauchy.py
+++ natter/Distributions/PCauchy.py
@@ -1,8 +1,9 @@
 from __future__ import division
 from natter.DataModule import Data
-from numpy import mean, sum, abs, log,zeros,atleast_2d,Inf,atleast_1d
+from numpy import mean, sum, abs, log,zeros,atleast_2d,Inf,atleast_1d, isnan, isinf,any
 
 from natter.Auxiliary.Utils import parseParameters
+from natter.Auxiliary.Numerics import trigamma
 from scipy.special import gamma, digamma, gammaln
 from Distribution import Distribution
 from warnings import warn
@@ -60,7 +61,7 @@ class PCauchy(Distribution):
 
     def dldtheta(self,dat):
         """
-        Evaluates the gradient of the distribution with respect to the primary parameters.
+        Evaluates the gradient of the loglikelihood with respect to the primary parameters.
 
         :param dat: Data on which the gradient should be evaluated.
         :type dat: DataModule.Data
@@ -71,12 +72,32 @@ class PCauchy(Distribution):
 
         n = float(self.param['n'])
         p = float(self.param['p'])
-
+        
         r = atleast_2d(sum(abs(dat.X)**p,axis=0))
         return n/p - (n+1)/p**2.0*(digamma((n+1)/p)-digamma(1/p)) + (n+1)/p**2.0*log(1.0 + r)\
-               - (n+1)*sum(abs(dat.X)**p * log(abs(dat.X)),axis=0) / p / (1.0 + r)
+               - (n+1)*sum(abs(dat.X)**p * log(abs(dat.X) + (abs(dat.X ) == 0.0)*1e-12),axis=0) / p / (1.0 + r)
 
 
+    def d2ldtheta2(self,dat):
+        """
+        Evaluates the second derivative of the loglikelihood with respect to the primary parameters.
+
+        :param dat: Data on which the gradient should be evaluated.
+        :type dat: DataModule.Data
+        :returns:   The gradient
+        :rtype:     numpy.array
+        
+        """
+
+        n = float(self.param['n'])
+        p = float(self.param['p'])
+        r = 1.0 + atleast_2d(sum(abs(dat.X)**p,axis=0))
+        dr = sum(abs(dat.X)**p * log(abs(dat.X) + (abs(dat.X ) == 0.0)*1e-12),axis=0)
+        d2r = sum(abs(dat.X)**p * log(abs(dat.X) + (abs(dat.X ) == 0.0)*1e-12)**2.0,axis=0)
+        
+        return -n/p**2.0 + 2*(n+1)/p**3.0 * (digamma((n+1)/p) - digamma(1/p)) \
+               + (n+1)/p**4.0*((n+1)*trigamma((n+1)/p) - trigamma(1/p)) \
+               - 2*(n+1)/p**3.0 * log(r) + 2.0*(n+1)*dr/p**2.0/r - (n+1)*d2r/p/r + (n+1)*dr**2.0/p/r**2.0
 
     
 
@@ -92,6 +113,7 @@ class PCauchy(Distribution):
         '''
 
         if 'p' in self.primary:
+            
             fprime = lambda p: mean(self.array2primary(atleast_1d(p)).dldtheta(dat))
             pold = Inf
             p = self.param['p']
@@ -99,8 +121,7 @@ class PCauchy(Distribution):
             while count < maxiter and abs(p-pold) > tol:
                 count += 1
                 pold = p
-                p = p/(1.0 -p*fprime(p))
-
+                p = p/(1.0 -p*fprime(p)*.1)
             if count == maxiter:
                 warn('PCauchy.estimate: Maximal number of iterations reached. Algorithm might not have converged(|dp|=%.4g)'\
                      %(abs(p-pold),))
